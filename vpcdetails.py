acc7e9288f7b6bd15ce5dbcd7849f963dceda47d
nuthalapatiashok <nuthalapatiashok@gmail.com>
2020-07-26 14:45:09 +0530

    Update vpcdetails.py

--- vpcdetails.py
+++ vpcdetails.py
@@ -1,8 +1,14 @@
 
 #List VPC
 import boto3
-ec2 = boto3.resource('ec2',region_name="us-west-1",aws_access_key_id= "AKIAVCPCFOO6I6LOEE2I", aws_secret_access_key= "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")
-vpc_client = boto3.client('ec2',region_name="us-west-1",aws_access_key_id= "AKIAVCPCFOO6I6LOEE2I", aws_secret_access_key= "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")
+import sys
+
+#region = sys.argv[1]
+accesskey = sys.argv[2]
+secretkey = sys.argv[3]
+
+ec2 = boto3.resource('ec2',aws_access_key_id= accesskey, aws_secret_access_key= secretkey)
+vpc_client = boto3.client('ec2',aws_access_key_id= accesskey , aws_secret_access_key= secretkey )
 response_vpc = vpc_client.describe_vpcs()
 #print response_vpc['Vpcs']
 for each_vpc in response_vpc['Vpcs']:
