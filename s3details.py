b19dbb9c6ee16d7be9a42f1f646cb29656bdbae8
nuthalapatiashok <nuthalapatiashok@gmail.com>
2020-07-26 14:38:04 +0530

    Update s3details.py

--- s3details.py
+++ s3details.py
@@ -1,8 +1,14 @@
 #List S3 bucket
 
 import boto3
+import sys
 
-s3_details = boto3.client('s3',region_name="us-west-1",aws_access_key_id= "AKIAVCPCFOO6I6LOEE2I", aws_secret_access_key= "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")
+region = sys.argv[1]
+accesskey = sys.argv[2]
+secretkey = sys.argv[3]
+
+
+s3_details = boto3.client('s3',region_name= region,aws_access_key_id= accesskey, aws_secret_access_key= secretkey)
 
 response_s3details = s3_details.list_buckets()
 #print (response_s3details['Buckets'])
